7ed7b45e44d1abc243cbe5a178b18e2a4d227559
Michael Lei <michaelleie0ca@gmail.com>
2023-06-25 01:00:54 -0700

    Add check for error

--- routes/api.ts
+++ routes/api.ts
@@ -5,8 +5,12 @@ const api = express.Router();
 
 api.post("/dream", async (req, res) => {
   const { dream } = req.body;
-  const interpretation = await generateDreamInterpretation(dream);
-  res.json(interpretation);
+  try {
+    const interpretation = await generateDreamInterpretation(dream);
+    res.json(interpretation);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default api;
