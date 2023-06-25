1f18a6cf345058b5e0fc708d68b511adde4bdab3
Michael Lei <michaelleie0ca@gmail.com>
2023-06-25 01:05:52 -0700

    Add cors

--- app.ts
+++ app.ts
@@ -2,10 +2,18 @@ import express from "express";
 import "dotenv/config";
 import logger from "morgan";
 import rateLimit from "express-rate-limit";
+import cors, { CorsOptions } from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,7 +23,7 @@ app.use(logger("dev"));
 
 import api from "./routes/api";
 
-app.use("/api", api);
+app.use("/api", apiLimiter, api);
 
 app.listen(PORT);
 
